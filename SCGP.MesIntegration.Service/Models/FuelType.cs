991bf36b6263214bb0c7947b90687b4e12478d3f
huyleauto <lequanghuyqn123@gmail.com>
2026-09-19 16:27:08 +0700

    Project creation

---
+++ SCGP.MesIntegration.Service/Models/FuelType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SCGP.MesIntegration.Service.Models;
+
+public partial class FuelType
+{
+    public int FuelTypeId { get; set; }
+
+    public string FuelName { get; set; } = null!;
+
+    public decimal? FuelLhv { get; set; }
+
+    public int UnitId { get; set; }
+
+    public string? FuelNameVn { get; set; }
+
+    public virtual ICollection<FuelFeedLog> FuelFeedLogs { get; set; } = new List<FuelFeedLog>();
+
+    public virtual Unit Unit { get; set; } = null!;
+}
--- SCGP.MesIntegration.Service/Program.cs
+++ SCGP.MesIntegration.Service/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using SCGP.MesIntegration.Service.Models;
+
 namespace SCGP.MesIntegration.Service
 {
     public class Program
@@ -7,10 +10,26 @@ namespace SCGP.MesIntegration.Service
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddJsonFile("appsettings.Secrets.json", optional: true, reloadOnChange: true);
+
+            #region Register Windows Service
+
+            builder.Host.UseWindowsService();
+
+            #endregion
+
+            #region MSSQL
+            var connString = builder.Configuration["MSSQL:connString"];
+
+            builder.Services.AddDbContextFactory<ProductionDbContext>(options =>
+            {
+                options.UseSqlServer(connString);
+            });
+            #endregion
 
             var app = builder.Build();
 
-            
+            app.MapGet("/", () => "SCGP.MesIntegration.Service is running...");
 
             app.Run();
         }
